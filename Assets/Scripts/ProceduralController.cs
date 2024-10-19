2208e4638414b9e7c8cdaa1c13af32bc448ce300
Felipe Aristizábal <138895922+Felipe-Aristizabal@users.noreply.github.com>
2024-10-19 16:24:32 -0500

    feat: the cameras could change when button is pressed

--- Assets/Scripts/ProceduralController.cs
+++ Assets/Scripts/ProceduralController.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 public class ProceduralController : MonoBehaviour
 {
     [SerializeField] private GameObject[] pointsBuildings;
     [SerializeField] private SOBuildingData buildingDataSO;
     
-    [SerializeField]private Transform spawnParent;
-    
+    [SerializeField] private Transform spawnParent;
+    [SerializeField] private GameObject noteCamera;
+
+    [SerializeField] private Button[] buttonsChangePreview = new Button[3];
+    private GameObject[] selectedBuildings = new GameObject[3];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,13 +29,56 @@ public class ProceduralController : MonoBehaviour
             buildingToPosisionate.transform.position = point.transform.position;
         }
 
-        GetRandomBuilding();
+        GetRandomBuildings();
+        AssignButtonCallbacks();
     }
 
-    private void GetRandomBuilding()
+    private void GetRandomBuildings()
     {
-        GameObject customerBuilding = spawnParent.GetChild(Random.Range(0, spawnParent.childCount)).gameObject;
-        customerBuilding.transform.GetChild(0).gameObject.SetActive(true);
-        Debug.Log(customerBuilding);
+        List<int> usedIndices = new List<int>(); 
+
+        for (int i = 0; i < 3; i++) 
+        {
+            int randomIndex;
+            do
+            {
+                randomIndex = Random.Range(0, spawnParent.childCount);
+            } while (usedIndices.Contains(randomIndex));
+
+            usedIndices.Add(randomIndex); 
+            GameObject customerBuilding = spawnParent.GetChild(randomIndex).gameObject;
+            selectedBuildings[i] = customerBuilding;
+            customerBuilding.transform.GetChild(0).gameObject.SetActive(true);
+
+            if (i == 0) 
+            {
+                ChangeCameraTransform(customerBuilding);
+            }
+
+            Debug.Log(customerBuilding.name); 
+        }
+    }
+
+    private void AssignButtonCallbacks()
+    {
+        for (int i = 0; i < buttonsChangePreview.Length; i++)
+        {
+            int index = i; 
+            buttonsChangePreview[i].onClick.AddListener(() => {
+                ChangeCameraTransform(selectedBuildings[index]); 
+                AnimateButton(buttonsChangePreview[index]); 
+            });
+        }
+    }
+
+    private void AnimateButton(Button button)
+    {
+        // This animation will be created by @SebastianEscobar
+    }
+
+    private void ChangeCameraTransform(GameObject selectedBuilding)
+    {
+        noteCamera.transform.position = selectedBuilding.transform.GetChild(1).position;
+        noteCamera.transform.rotation = selectedBuilding.transform.GetChild(1).rotation;
     }
 }
